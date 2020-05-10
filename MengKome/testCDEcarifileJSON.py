f4abc039d60f3edc8952d4a93702c073967c9f5f
Rosli Talib <xixa01@yahoo.co.uk>
2020-05-10 23:23:24 +0100

    2020.05.10 start code sqlite3 to JSON

---
+++ MengKome/testCDEcarifileJSON.py
@@ -0,0 +1,19 @@
+chromedatadir = "chrome-data"
+
+import os
+import shutil
+import sys
+import ctypes
+import sqlite3
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(chromedatadir):
+    for file in f:
+        if file=='Cookies':
+            files.append(os.path.join(r, file))
+
+# for f in files:
+#     print(f)
+if len(files) >= 1:
+    print(files[0])
